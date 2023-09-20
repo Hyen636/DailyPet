64fdeab6e4bdb8ebba07d7831cd66c231480ab99
kangkyeongseo <kks_big@naver.com>
2023-09-20 14:02:07 +0900

    add: Button 컴포넌트 추가(#3)

---
+++ daily-pet/src/components/Form/Button/Button.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const props = defineProps<{ isLarge: boolean }>()
+</script>
+
+<template>
+  <button :class="[{ 'btn--large': isLarge }, 'btn']">
+    <slot></slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  cursor: pointer;
+  padding: 1.2rem 1.6rem;
+  border: none;
+  border-radius: 0.4rem;
+  background-color: #ff8125;
+  color: #fff;
+  font-weight: bold;
+  &:hover {
+    background-color: #ff5c00;
+  }
+}
+.btn--large {
+  width: 100%;
+}
+</style>
