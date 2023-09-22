34a55ea42bb236daa443c196e26d204c6b726c7a
kangkyeongseo <kks_big@naver.com>
2023-09-22 14:53:39 +0900

    add: Avatar 컴포넌트 추가(#8)

---
+++ daily-pet/src/components/Profile/ProfileHeader/Avatar/Avatar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({ images: Object })
+</script>
+
+<template>
+  <div class="avatar-container">
+    <img class="avatar-container__avatar" v-for="image in images" :src="image" alt="avatar" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.avatar-container__avatar {
+  cursor: pointer;
+  position: relative;
+  width: 8rem;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  box-shadow: 0rem 0.4rem 0.4rem 0rem rgba(0, 0, 0, 0.25);
+  &:first-child {
+    z-index: 10;
+    position: absolute;
+    transform: translateX(-6rem);
+  }
+}
+</style>
