0634f9f65b2df035f167349e190e22e25dab97ac
kangkyeongseo <kks_big@naver.com>
2023-09-22 14:55:51 +0900

    add: ProfileContainer 컴포넌트 추가(#8)

---
+++ daily-pet/src/components/Profile/ProfileContainer/ProfileContainer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import PetType from './PetType/PetType.vue'
+import PetInfo from './PetInfo/PetInfo.vue'
+
+const props = defineProps({ type: String, gender: String, age: Number, weight: Number })
+</script>
+
+<template>
+  <div class="profile-container">
+    <PetType :type="type" />
+    <PetInfo :gender="gender" :age="age" :weight="weight" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.profile-container {
+  transform: translateY(-4rem);
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  padding: 3rem 3.5rem;
+  background-color: #fff;
+  border-radius: 2rem;
+  box-shadow: 0rem 0.3rem 1rem 0rem rgba(0, 0, 0, 0.15);
+}
+</style>
