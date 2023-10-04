9b1d1cda02a6c4fc103964006ba85ba3a87adb9c
kangkyeongseo <kks_big@naver.com>
2023-10-04 13:54:10 +0900

    add: 아이콘 추가(#11)

---
+++ daily-pet/src/assets/icon/CheckIcon.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="15" viewBox="0 0 20 17" fill="none">
+    <path
+      d="M6.56448 16.4271L0 9.86258L2.99154 6.87104L6.56448 10.4545L17.0085 0L20 2.99154L6.56448 16.4271Z"
+      fill="#FF8125"
+    />
+  </svg>
+</template>
+
+<style lang="scss" scoped></style>
