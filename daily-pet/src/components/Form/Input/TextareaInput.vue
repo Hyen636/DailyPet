347ec5a239f022d1938a7ab4b42c246e32354852
Hyein Jung <penguin636@naver.com>
2023-10-10 14:47:42 +0900

    add: Textarea 컴포넌트 추가(#14)

---
+++ daily-pet/src/components/Form/Input/TextareaInput.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const resizeHeight = (e) => {
+  e.target.style.height = 'auto'
+  e.target.style.height = e.target.scrollHeight + 'px'
+}
+</script>
+
+<template>
+  <div class="textarea-box">
+    <slot></slot>
+    <textarea
+      type="textarea"
+      id="textarea"
+      class="textarea"
+      placeholder="내용을 입력해주세요."
+      @input="resizeHeight"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.textarea-box {
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 100%;
+  gap: 1rem;
+}
+.textarea {
+  resize: none;
+  width: 100%;
+  aspect-ratio: 1/0.5;
+  padding: 1.5rem;
+  gap: 1rem;
+  border-radius: 1rem;
+  border: 0.1rem solid #cfcfcf;
+  background: #fff;
+  overflow: hidden;
+  outline-color: #ff8125;
+}
+</style>
